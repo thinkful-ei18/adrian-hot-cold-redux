1672315329c29a1c222874eb469d3b30fcabc6c1
Adrian <adriantoddross@gmail.com>
2018-03-02 16:11:21 -0500

    reorganize code

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,23 +1,34 @@
+// adds a guess to guess history
 export const GUESS_ADD = 'GUESS_ADD';
 export const guessAdd = guess => ({
   type: GUESS_ADD,
   guess
 });
 
+// renders feedback to the user
 export const GUESS_FEEDBACK =  'GUESS_FEEDBACK';
 export const guessFeedback = guess => ({
   type: GUESS_FEEDBACK,
   guess
 });
 
+// handles feedback displayed above the guess history counter.
 export const GUESS_STATUS = 'GUESS_STATUS';
 export const guessStatus = guess => ({
   type: GUESS_STATUS,
   guess
 });
 
-export const GUESS_SECRET = 'GUESS_SECRET';
+// Generates the secret number at beginning of game.
+export const GUESS_CORRECTANSWER = 'GUESS_CORRECTANSWER';
 export const guessSecret = guess => ({
-  type: GUESS_SECRET,
+  type: GUESS_CORRECTANSWER,
+  guess
+});
+
+// Lets user choose WHAT? or NEW GAME.
+export const GUESS_NAVIGATION = 'GUESS_NAVIGATION';
+export const guessNavigation = guess => ({
+  type: GUESS_NAVIGATION,
   guess
 });
