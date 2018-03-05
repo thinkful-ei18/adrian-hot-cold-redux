f0844a0193a9e487edaa81de0199a166f693dc3a
Adrian <adriantoddross@gmail.com>
2018-03-05 14:57:35 -0500

    route this.state to redux-store props

--- src/components/game.js
+++ src/components/game.js
@@ -29,41 +29,40 @@ export class Game extends React.Component {
   }
 
   makeGuess(guess) {
-    // THIS VALIDATION HAS BEEN ADDED TO THE REDUCER!
+  this.props.dispatch(guessAdd(guess));
 
-    this.props.dispatch(guessAdd(guess));
-    guess = parseInt(guess, 10);
+    // guess = parseInt(guess, 10);
 
     // if (isNaN(guess)) {
     //   this.props.dispatch(guessAdd({feedback: 'Please enter a valid number'}));
     //   return;
     // }
 
-    const difference = Math.abs(guess - this.state.correctAnswer);
+    // const difference = Math.abs(guess - this.props.correctAnswer);
 
-    let feedback;
-    if (difference >= 50) {
-      feedback = 'You\'re Ice Cold...';
-    } else if (difference >= 30) {
-      feedback = 'You\'re Cold...';
-    } else if (difference >= 10) {
-      feedback = 'You\'re Warm.';
-    } else if (difference >= 1) {
-      feedback = 'You\'re Hot!';
-    } else {
-      feedback = 'You got it!';
-    }
+    // let feedback;
+    // if (difference >= 50) {
+    //   feedback = 'You\'re Ice Cold...';
+    // } else if (difference >= 30) {
+    //   feedback = 'You\'re Cold...';
+    // } else if (difference >= 10) {
+    //   feedback = 'You\'re Warm.';
+    // } else if (difference >= 1) {
+    //   feedback = 'You\'re Hot!';
+    // } else {
+    //   feedback = 'You got it!';
+    // }
 
-    this.setState({
-      feedback,
-      guesses: [...this.state.guesses, guess]
-    });
+    // this.setState({
+    //   feedback,
+    //   // guesses: [...this.state.guesses, guess]
+    // });
 
     // We typically wouldn't touch the DOM directly like this in React
     // but this is the best way to update the title of the page,
     // which is good for giving screen-reader users
     // instant information about the app.
-    document.title = feedback ? `${feedback} | Hot or Cold` : 'Hot or Cold';
+    // document.title = feedback ? `${feedback} | Hot or Cold` : 'Hot or Cold';
   }
 
   generateAuralUpdate() {
@@ -84,7 +83,7 @@ export class Game extends React.Component {
   }
 
   render() {
-    const { feedback, guesses, auralStatus } = this.state;
+    const { feedback, guesses, auralStatus } = this.props;
     const guessCount = guesses.length;
 
     return (
