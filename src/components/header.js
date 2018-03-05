9595a57b12345a373223c1bdc46098103b136303
Adrian <adriantoddross@gmail.com>
2018-03-05 16:22:02 -0500

    dispatch updateAuralStatus in topnav prop

--- src/components/header.js
+++ src/components/header.js
@@ -5,30 +5,14 @@ import TopNav from './top-nav';
 
 import './header.css';
 
-import { restartGame } from '../actions';
+import { restartGame, updateAuralStatus } from '../actions';
 
 function Header(props) {
-
-  function generateAuralUpdate () {
-    const { guesses, feedback } = props;
-
-    // If there's not exactly 1 guess, we want to
-    // pluralize the nouns in this aural update.
-    const pluralize = guesses.length !== 1;
-
-    let  auralStatus = `Here's the status of the game right now: ${feedback} You've made ${guesses.length} ${pluralize ? 'guesses' : 'guess'}.`;
-
-    if (guesses.length > 0) {
-      auralStatus += ` ${pluralize ? 'In order of most- to least-recent, they are' : 'It was'}: ${guesses.reverse().join(', ')}`;
-    }
-
-    return auralStatus;
-  }
 
   return (
     <header>
       <TopNav
-        onGenerateAuralUpdate={() => generateAuralUpdate()}
+        onGenerateAuralUpdate={() => props.dispatch(updateAuralStatus())}
         onRestartGame={() => props.dispatch(restartGame())}
       />
       <h1>HOT or COLD</h1>
@@ -36,10 +20,4 @@ function Header(props) {
   );
 }
 
-const mapStateToProps = (state) => ({
-  guesses: state.guesses,
-  feedback: state.feedback,
-  auralStatus: state.auralStatus
-})
-
-export default connect(mapStateToProps)(Header);
+export default connect()(Header);
