e6ee8c788a4e0c1032e2af7b9b41e6b985e72422
Adrian <adriantoddross@gmail.com>
2018-03-05 16:43:28 -0500

    delete jest test

--- tests/reducer-test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import reducers from '../src/reducers/index';
-
-test('reducers', () => {
-  let state;
-  state = reducers({guesses:[54],feedback:'You\'re Cold...',auralStatus:'',correctAnswer:7}, {type:'UPDATE_AURALSTATUS'});
-  expect(state).toEqual({guesses:[54],feedback:'You\'re Cold...',auralStatus:'Here\'s the status of the game right now: You\'re Cold... You\'ve made 1 guess. It was: 54',correctAnswer:7});
-});
